14d83030aad0ff0babd4a7d7a89eadab58cf1260
xuanpt2 <bktncn7m@gmail.com>
2024-07-17 16:28:52 +0800

    add: 通过群组id获取其订阅内容

--- src/main/java/com/xuanpt2/slogjava/controller/BlogGroupsController.java
+++ src/main/java/com/xuanpt2/slogjava/controller/BlogGroupsController.java
@@ -3,12 +3,10 @@ package com.xuanpt2.slogjava.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.xuanpt2.slogjava.dto.BlogGroupDto;
 import com.xuanpt2.slogjava.dto.BlogMetaDto;
-import com.xuanpt2.slogjava.entity.BlogGroupInfo;
-import com.xuanpt2.slogjava.entity.BlogGroups;
-import com.xuanpt2.slogjava.entity.BlogRelationship;
-import com.xuanpt2.slogjava.entity.BlogRssSub;
+import com.xuanpt2.slogjava.entity.*;
 import com.xuanpt2.slogjava.service.IBlogGroupInfoService;
 import com.xuanpt2.slogjava.service.IBlogGroupsService;
+import com.xuanpt2.slogjava.service.IBlogRssContentsService;
 import com.xuanpt2.slogjava.service.IBlogRssSubService;
 import com.xuanpt2.slogjava.vo.TResponseVo;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -16,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -39,6 +38,9 @@ public class BlogGroupsController {
 
     @Autowired
     private IBlogRssSubService blogRssSubService;
+
+    @Autowired
+    private IBlogRssContentsService blogRssContentsService;
 
     /**
      * 获取所有群组信息
@@ -65,6 +67,24 @@ public class BlogGroupsController {
 
 
         return TResponseVo.success(groupDtoList);
+    }
+
+    @PostMapping("/getGroupContentsByGroupId")
+    public TResponseVo<List<BlogRssContents>> getGroupContentsByGroupId(@RequestBody Map<String, Object> map){
+        List<BlogRssContents> list = new ArrayList<>();
+        try {
+             List<Integer> ridList =
+                     BlogGroupInfo.toRidList(blogGroupInfoService.list(new QueryWrapper<BlogGroupInfo>().eq("group_id",
+                    (Serializable) map.get("groupId"))));
+
+            for (Integer rid :
+                    ridList) {
+                list.addAll(blogRssContentsService.list(new QueryWrapper<BlogRssContents>().eq("rid", rid)));
+            }
+            return TResponseVo.success(list);
+        }catch (Exception e){
+            return TResponseVo.error(500, e.getMessage());
+        }
     }
 
     /**
